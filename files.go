453ccc246a148d8cd41e991e47af5cc79f989002
Vasiliy Solovey <solovey@cinegy.com>
2017-09-08 17:55:27 +0300

    choose random available port for server binding

--- files.go
+++ files.go
@@ -80,6 +80,7 @@ func init() {
 	serviceFiles = map[string][]byte{
 		pythonService: []byte(`
 import logging
+import sys
 
 try:
     import cStringIO as StringIO
@@ -159,7 +160,7 @@ def overview():
 
 
 if __name__ == '__main__':
-    app.run(port=1414)
+    app.run(port=sys.argv[1])
 `),
 		asciidoc: bytes.Replace([]byte(`
 #!/usr/bin/env python
@@ -6729,11 +6730,11 @@ ensure
   $stderr = previous_stderr
 end
 
-enable :quiet, :lock
-disable :logging, :threaded
+enable :quiet
+disable :logging
 set :environment, :production
 set :bind, 'localhost'
-set :port, 1515
+set :port, ARGV[0]
 
 get '/' do
   response = { status: 'running', asciidoctor: adoctor_supported }
